3c1c9ba9c42c3053a5d662291aed9bdb51b612e9
Chris Leblanc <cleblanc@arthrex.com>
2016-08-02 07:13:19 -0600

    initial commit

---
+++ api2/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message string
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":5000", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	profile := Response{"Welcome to api 2"}
+
+	js, err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Create service hub & private networks: cd gateway && make gateway
+Start api1 & api2 via convox
+
+
+visit api1.127.0.0.1.xip.io && api2.127.0.0.1.xip.io in browser
+
+visit http://kong-dashboard.127.0.0.1.xip.io/ to see kong setup
